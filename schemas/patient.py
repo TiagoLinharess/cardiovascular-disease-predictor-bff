bd1677541e5ff060bf3ec70065f25f7ffd90df41
Tiago Linhares <tiagolinharessouza@gmail.com>
2024-09-27 11:28:13 -0300

    Corrige Schema

--- schemas/patient.py
+++ schemas/patient.py
@@ -1,16 +1,16 @@
 from pydantic import BaseModel
 
 class PatientSchema(BaseModel):
-    age: float = 63
-    sex: int = 1
-    cp: int = 2
-    trestbps: float = 145.0
-    chol: float = 204
-    fbs: int = 1
-    restecg: int = 2
-    thalach: float = 108
-    exang: int = 1
-    oldpeak: float = 2.6
-    slope: float = 2
-    ca: float = 3
-    thal: int = 7
+    age: float
+    sex: int
+    cp: int
+    trestbps: float
+    chol: float
+    fbs: int
+    restecg: int
+    thalach: float
+    exang: int
+    oldpeak: float
+    slope: float
+    ca: float
+    thal: int
